e51f57f7715f43aade62ded51ed15f14924ddded
Horlakz <horlakz@protonmail.com>
2022-02-23 07:35:37 +0100

    added user authentication

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,12 @@
 import { useState, useEffect } from "react";
 import { FaSignInAlt } from "react-icons/fa";
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { FaUser } from "react-icons/fa";
+import { login, reset } from '../features/auth/authSlice'
+import Spinner from "../components/Spinner";
+
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -8,6 +15,25 @@ function Login() {
   });
 
   const { email, password } = formData;
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector((state) => state.auth)
+
+  useEffect(() => {
+    if(isError) {
+      toast.error(message)
+    }
+
+    if (isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -19,6 +45,16 @@ function Login() {
   const onSubmit = (e) => {
     e.preventDefault()
   }
+
+  const userData = {
+    email,
+    password 
+  }
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
 
   return (
     <>
