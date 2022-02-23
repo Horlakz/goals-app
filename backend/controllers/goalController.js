2d33aaa0d54fdccfe4426df0839252e493191655
Horlakz <horlakz@protonmail.com>
2022-02-23 07:35:08 +0100

    removed re used code

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -38,16 +38,14 @@ const updateGoal = asyncHandler(async (req, res) => {
     throw new Error("Goal not found");
   }
 
-  const user = await User.findById(req.user.id)
-
   // check for user
-  if (!user) {
+  if (!req.user) {
     res.status(401)
     throw new Error('User not found')
   }
 
   // make sure the loggd in user matches the goal user
-  if(goal.user.toString() !== user.id) {
+  if(goal.user.toString() !== req.user.id) {
     res.status(401)
     throw new Error('User not authorised')
   }
@@ -69,17 +67,15 @@ const deleteGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Goal not found");
   }
-  
-  const user = await User.findById(req.user.id)
 
   // check for user
-  if (!user) {
+  if (!req.user) {
     res.status(401)
     throw new Error('User not found')
   }
 
   // make sure the loggd in user matches the goal user
-  if(goal.user.toString() !== user.id) {
+  if(goal.user.toString() !== req.user.id) {
     res.status(401)
     throw new Error('User not authorised')
   }
